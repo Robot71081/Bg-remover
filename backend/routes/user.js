f24a9cb774c5e9b3e08c2598c85df1514a589270
Rohit Pawar <rohitpawar5062@gmail.com>
2024-10-28 00:21:56 +0530

    fixed the bug

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,7 @@ import { clerkwebHooks } from '../controllers/user.js';
 const userRoutes = express.Router();
 
 // Define the POST route for webhooks
-userRoutes.post('/webhooks',clerkwebHooks);
+userRoutes.get('/webhooks',clerkwebHooks);
 
 export default userRoutes;
 
