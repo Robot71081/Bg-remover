6243ad8e950e5e5f95f43b51ed8026d44ed866e2
Rohit Pawar <rohitpawar5062@gmail.com>
2024-10-28 00:32:27 +0530

    gdfg

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ async function startServer() {
     // Middleware to parse JSON requests
     app.use(express.json());
     app.use(cors());
+    app.use((req, res, next) => {
+        res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self' https://vercel.live;");
+        next();
+    });
 
 
     app.use('/api/user', userRoutes); // Make sure this is included
