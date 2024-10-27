adabd4cb2d73f8f5d04b867b317871611929a821
Rohit Pawar <rohitpawar5062@gmail.com>
2024-10-28 00:41:21 +0530

    dfgd

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongoDB.js';
-import userRoutes from './routes/user.js';
+import userRouter from './routes/user.js';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -13,16 +13,10 @@ async function startServer() {
     // Middleware to parse JSON requests
     app.use(express.json());
     app.use(cors());
-    app.use((req, res, next) => {
-        res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self' https://vercel.live;");
-        next();
-    });
 
-
-    app.use('/api/user', userRoutes); // Make sure this is included
     // Define API routes
-    app.get('/', );
-   
+    app.get('/', (req, res) => res.send('API working'));
+    app.use('/api/user', userRouter); // Make sure this is included
 
     // Start the server
     app.listen(PORT, () => console.log('Server running on port ' + PORT));
